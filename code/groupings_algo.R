bd1dabe1071858d084fb5a108d7f8b41c1d6d254
TomBearpark <t.bearpark@googlemail.com>
2023-05-07 18:12:27 -0400

    trying Bonhomme Manresa 2015 algorithm 1

--- code/groupings_algo.R
+++ code/groupings_algo.R
@@ -25,29 +25,76 @@ df <- tibble(i = 1:N,
              y = D * y1 + (1-D) * y0
              )
 
+# Check what we would recover if we had full information 
 feols(y ~ D, df, vcov = "hetero")
+feols(y ~ D | G, df, vcov = "hetero")
 feols(y ~ D + G, df, vcov = "hetero")
 feols(y ~ -1 + D + G + D*G, df, vcov = "hetero")
 
-
-
-
+# What we would observe in the real world... 
 df.obs <- df %>% select(i, y, D)
-
-df.obs$kk <- kmeans(df.obs$y, centers = 2)$cluster -1 
-
-feols(y ~ -1 + D + G + D*G, df, vcov = "hetero")
 
 # B&M ---------------------------------------------------------------------
 
-s=0
-beta=.9
-alpha=sample(c(0, 1), N, replace = TRUE)
+# Tolerance for convergence
+tol <- 0.0001
+div <- 100000
+# Matrices to store results in 
+max.iter <- 1000
+beta.mat  <- matrix(nrow = max.iter, ncol = 1)
+alpha.mat <- matrix(nrow = max.iter, ncol = 2)
 
-g1=
+# 1. Initial guess
+
+s = 0
+beta.mat[1, ]  <- c(.9)
+alpha.mat[1, ] <- c(0,  1)
 
 
+while(div > tol){
+  
+  s <- s + 1
+  
+  beta <- beta.mat[s, ]
+  alpha <- alpha.mat[s, ]
+  
+  # 2. Assignment step
+  df.obs$e1 = (df.obs$y - (beta[1]*df.obs$D + alpha[1]))^2
+  df.obs$e2 = (df.obs$y - (beta[1]*df.obs$D + alpha[2]))^2
+  
+  df.obs$g  = ifelse(df.obs$e1 < df.obs$e2, 0, 1)
+  
+  # 3. Update step 
+  m <- feols(y ~ D | g, data = df.obs)
+  
+  # Store new coefficients
+  beta.mat[s+1, ]  <- coef(m)[1]
+  alpha.mat[s+1, ] <- drop(fixest::fixef(m)$g)
+  
+  # Check how close we are to convergence 
+  div <- max(c(beta.mat[s,]-beta.mat[s+1,], alpha.mat[s,]-alpha.mat[s+1,]))
+  print(div)
+  
+}
+
+plot(beta.mat[!is.na(beta.mat)])
+
+
+
+
+
+# Scraps from here onwards!
 # trying Gs ---------------------------------------------------------------
+
+
+df.obs$e <- feols(y ~ D, df.obs)$resid
+
+df$kk <- kmeans(df.obs$e, centers = 2)$cluster - 1
+feols(y ~ -1 + D + kk + D*kk, df, vcov = "hetero")
+feols(G ~ kk, df)
+
+
+plot(kk)
 
 # Candidate grouping vector
 df.obs$G.t <- sample(c(0, 1), N, replace = TRUE)
